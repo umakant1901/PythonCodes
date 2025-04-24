177a08d9761a3be25498455ddeae4c72431d8f1f
Umakant1901 <ukp.linux@gmail.com>
2025-04-24 20:11:59 +0530

    float data type part 1

---
+++ basics/float_datatypecode.py
@@ -0,0 +1,18 @@
+'''
+float data type:
+
+
+
+'''
+
+weight= 123.45
+weight2 = 123.45
+weight3 = weight
+weight4 = 12.12
+print(weight)
+print(type(weight))
+
+print(id(weight))
+print(id(weight2))
+print(id(weight3))
+print(id(weight4))
