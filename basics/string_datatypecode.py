3db9c7f0b509c182660b9408f4f8cb416f88b11e
Umakant1901 <ukp.linux@gmail.com>
2025-04-25 18:43:12 +0530

    started day2 code

--- basics/string_datatypecode.py
+++ basics/string_datatypecode.py
@@ -10,3 +10,23 @@ multiline_str = '''Hello,
 Umakant, How are you?
 Hope you are good...!'''
 print(multiline_str)
+
+str1 = 'hello'
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+print(str1[-1])
+print(str1[-2])
+
+print("Length of str1 is : ", len(str1))
+
+# slice in string : it's operator. [] to retrieve the slice of string.
+para1 = " logs collected at /mnt/logs/postgres/consents/file1.log find logs and check the issues."
+para2 = " logs collected at /mnt/logs/snowflake/consents/file2.log find logs and check the issues."
+
+log_start_index = para1.index("/mnt/logs/")
+log_end_index = para1.index('.log')
+print(para1[log_start_index:log_end_index+4])
+print(para2[para2.index("/mnt/log"):para2.index(".log")+4])
